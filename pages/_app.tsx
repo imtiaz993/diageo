860e2abd61535ee31ac68b7cb40a51fe3135e379
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2023-10-30 14:42:24 +0500

    added scripts and stylesheets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,79 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-    <Script
-      src="/css/bootstrap.min.css"
-      strategy="afterInteractive"
-      noModule
-    />
-    <Script
-      src="/css/animate.min.css"
-      strategy="afterInteractive"
-      noModule
-    />
-    <Script
-      src="/css/owl.carousel.css"
-      strategy="afterInteractive"
-      noModule
-    />
-    <link rel="stylesheet" type="text/css" href="/css/product.min.css" />
-    <Script src="/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
-    <Script src="/js/jquery-3.7.1.min.js" strategy="beforeInteractive" />
-    <Script src="/js/owl.carousel.min.js" strategy="beforeInteractive" />
-    <Script src="/js/wow.min.js" type="text/javascript" strategy="beforeInteractive" />
-    <Script src="/js/lazyload.min.js" type="text/javascript" strategy="beforeInteractive" />
-    <Script src="/js/product.min.js" type="text/javascript" strategy="beforeInteractive" />
+<>
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"
+          strategy="beforeInteractive"
+          integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"
+          strategy="beforeInteractive"
+          integrity="sha512-9CWGXFSJ+/X0LWzSRCZFsOPhSfm6jbnL+Mpqo0o8Ke2SYr8rCTqb4/wGm+9n13HtDE1NQpAEOrMecDZw4FXQGg=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"
+          strategy="beforeInteractive"
+          integrity="sha512-X/YkDZyjTf4wyc2Vy16YGCPHwAY8rZJY+POgokZjQB2mhIRFJCckEGc6YyX9eNsPfn0PzThEuNs+uaomE5CO6A=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+          strategy="beforeInteractive"
+          integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/jquery_lazyload/1.9.7/jquery.lazyload.min.js"
+          strategy="beforeInteractive"
+          integrity="sha512-yP/50WfAXV8JyDvqj8+kX3F/JVSYK4U6PYCriyH4xJvc5YtXbcBLaO2e/4ebu/obGWIObxDKkQQAfzRs0L121A=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"
+          strategy="beforeInteractive"
+          integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css"
+            integrity="sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+            integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css"
+            integrity="sha512-RWhcC19d8A3vE7kpXq6Ze4GcPfGe3DQWuenhXAbcGiZOaqGojLtWwit1eeM9jLGHFv8hnwpX3blJKGjTsf2HxQ=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+        </Head>
+      </>
+
+
 
     <Component {...pageProps} />
   </>
